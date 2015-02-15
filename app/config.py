e51f4780219668b14f2569f27e0efbec4e841fca
rayray1 <rupiray@yahoo.com>
2015-02-15 14:58:49 +0000

    flask task manager

---
+++ app/config.py
@@ -0,0 +1,15 @@
+# config.py - holds the app's settings and configuration variables
+
+import os
+
+# grabs the folder where the script runs
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+CSRF_ENABLED = True
+SECRET_KEY = 'my_precious'
+
+# defines the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
---
+++ app/run.py
@@ -0,0 +1,5 @@
+# run.py - starts the Flask server
+from views import app
+
+
+app.run(debug=True)
